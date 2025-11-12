24ed11d586fd8c966853f5bcb1b434fc2ed41825
DenaAlharbi <s202250560@kfupm.edu.sa>
2025-11-12 12:17:30 +0300

    las edited

--- 6-3-express-request-data/server.js
+++ 6-3-express-request-data/server.js
@@ -125,11 +125,11 @@ app.get("/echo", (req,res)=>{ const {name, age} = req.query;
     if( !name|| !age) {
     return res.status(400).json({ ok:false, error:"name & age required" })}
 
-    return res.json({ ok:true, name, age, msg:"Hello <name>, you are <age>" })});
+    return res.json({ ok:true, name, age, msg:'Hello ${name}, you are ${age}' })});
 
 // Route params: /profile/First/Last
 app.get("/profile/:first/:last", (req,res)=>{ const { first, last } = req.params;
-    return res.json({ ok:true, fullName: "${first} ${last}"});
+    return res.json({ ok:true, fullName: '${first} ${last}'});
 
 });
 /*   - create app.param("userId", ...)
